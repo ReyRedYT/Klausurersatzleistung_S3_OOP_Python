391fac3bfa8f5baec27b909983263e1e64b594bc
Malteeeee <49271270+EinsMalte@users.noreply.github.com>
2024-12-01 21:05:35 +0100

    Add transfer method and enhance transaction display in Konto class

--- Neu/Konto.py
+++ Neu/Konto.py
@@ -42,15 +42,29 @@ class Konto:
         self.__buchungen.append((betrag, verwendungszweck))
 
     def einzahlen(self, betrag):
-        self.buchen(betrag, "Einzahlung")
+        self.buchen(betrag, "Einzahlung") # Typecheck ist in buchen() implementiert
     
     def auszahlen(self, betrag):
-        self.buchen(-betrag, "Auszahlung")
+        self.buchen(-betrag, "Auszahlung") # Typecheck ist in buchen() implementiert
+
+    def ueberweisen(self, empfaenger, betrag):
+        if not typecheck(empfaenger, [Konto]):
+            raise TypeError("Der Empfänger muss ein Konto sein.")
+        if not typecheck(betrag, [int, float]):
+            raise TypeError("Der Betrag muss eine Zahl sein.")
+        if betrag < 0:
+            raise ValueError("Der Betrag darf nicht negativ sein.")
+        
+        self.buchen(-betrag, f"Überweisung an {empfaenger.inhaber()}")
+        empfaenger.buchen(betrag, f"Überweisung von {self.__inhaber}")
 
     def buchungen_anzeigen(self):
         print("Buchungen für:", self.__inhaber, self.__iban)
+        print("Num. Betrag       - Verwendungszweck")
         for i in range(len(self.__buchungen)):
-            print(f"{(i+1):2.0f}. {self.__buchungen[i][0]:8.2f} EUR - {self.__buchungen[i][1]}")
+            print(f"{(i+1):3.0f}. {self.__buchungen[i][0]:8.2f} EUR - {self.__buchungen[i][1]}")
+        print("---------")
+        print(f"=    {self.__guthaben:8.2f} EUR",end="\n\n")
     
     def __str__(self):
         return f"[Konto] Inhaber: {self.__inhaber}, IBAN: {self.__iban}, Guthaben: {self.__guthaben} EUR"
@@ -60,4 +74,7 @@ if __name__ == "__main__":
     konto1 = Konto("Arasp Shojai", 1000)
     konto1.einzahlen(500)
     konto1.auszahlen(200)
+    konto2 = Konto("Malte Haan", 2000)
+    konto1.ueberweisen(konto2, 300)
     konto1.buchungen_anzeigen()
+    konto2.buchungen_anzeigen()
