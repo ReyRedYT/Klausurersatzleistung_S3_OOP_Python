c7529904b5fc6c7ce32fd306430f9b723f676515
Malteeeee <49271270+EinsMalte@users.noreply.github.com>
2024-12-01 20:58:25 +0100

    Add transaction tracking and methods for deposits and withdrawals in Konto class

--- Neu/Konto.py
+++ Neu/Konto.py
@@ -16,6 +16,7 @@ class Konto:
         self.__inhaber = inhaber
         self.__guthaben = startguthaben
         self.__iban = self.generiere_iban()
+        self.__buchungen = []
     
     def generiere_iban(self):
         return 'DE' + ''.join([str(random.randint(0, 9)) for _ in range(20)])
@@ -31,20 +32,32 @@ class Konto:
         return self.__iban
 
     # Buchungsmethoden
-    def buchen(self, betrag):
+    def buchen(self, betrag, verwendungszweck=""):
         if not typecheck(betrag, [int, float]):
             raise TypeError("Der Betrag muss eine Zahl sein.")
         if self.__guthaben + betrag < 0:
             raise ValueError("Das Guthaben darf nicht negativ werden.")
         
         self.__guthaben += betrag
+        self.__buchungen.append((betrag, verwendungszweck))
 
+    def einzahlen(self, betrag):
+        self.buchen(betrag, "Einzahlung")
+    
+    def auszahlen(self, betrag):
+        self.buchen(-betrag, "Auszahlung")
+
+    def buchungen_anzeigen(self):
+        print("Buchungen für:", self.__inhaber, self.__iban)
+        for i in range(len(self.__buchungen)):
+            print(f"{(i+1):2.0f}. {self.__buchungen[i][0]:8.2f} EUR - {self.__buchungen[i][1]}")
+    
     def __str__(self):
         return f"[Konto] Inhaber: {self.__inhaber}, IBAN: {self.__iban}, Guthaben: {self.__guthaben} EUR"
 
 
 if __name__ == "__main__":
     konto1 = Konto("Arasp Shojai", 1000)
-    print(konto1)  # [Konto] Inhaber: Arasp Shojai, IBAN: DE12345678901234567890, Guthaben: 1000 EUR
-    konto1.buchen(500)
-    print(konto1)  # [Konto] Inhaber: Arasp Shojai, IBAN: DE12345678901234567890, Guthaben: 1500 EUR
+    konto1.einzahlen(500)
+    konto1.auszahlen(200)
+    konto1.buchungen_anzeigen()
